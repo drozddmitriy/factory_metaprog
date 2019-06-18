ecfea600bf9c8119a45e1b04701398bd26513d3c
drozd <drozd.dmitriy.etc@gmail.com>
2019-06-18 18:29:43 +0300

    Add ArgumentError

--- lib/factory.rb
+++ lib/factory.rb
@@ -36,6 +36,8 @@ class Factory
           hash.each do |key, value|
             instance_variable_set("@#{key}", value)
           end
+        else
+          raise ArgumentError, 'Wrong number of arguments'
         end
       end
 
